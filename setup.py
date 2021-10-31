b17fd157939ab89a0f2dc015aa02ec5a0fbbb07b
Roman <klymroman@protonmail.com>
2021-10-31 19:33:50 +0200

    updated dist

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="browserpasswords",
-    version="0.0.2",
+    version="0.0.3",
     author="Roman Klym",
     author_email="klymromanr@protonmail.com",
     description="Collects login information from Chrome, Mozilla & Brave",
